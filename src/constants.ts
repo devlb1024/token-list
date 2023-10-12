096821325b4695e5effc4ff82141cbcec3c91422
libin <libin52005@gmail.com>
2023-10-12 18:42:56 +0800

    ci: ddddfff

--- src/constants.ts
+++ src/constants.ts
@@ -11,4 +11,10 @@ export const LISTS = {
     logoURI: "https://kurama-token.netlify.app/images/kurama/0x5c1fe01d69d2657455b6d4ae61500f2f42353e10.png",
     sort: false,
   },
+  "kurama-pre": {
+    name: "Kurama",
+    keywords: ["kurama", "default"],
+    logoURI: "https://kurama-token.netlify.app/images/kurama/0x5c1fe01d69d2657455b6d4ae61500f2f42353e10.png",
+    sort: false,
+  },
 } as const;
