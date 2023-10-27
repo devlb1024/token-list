faf931af71e7d9f0b5279bfc153823f488144a02
libin <libin52005@gmail.com>
2023-10-27 11:39:13 +0800

    增加tomo 正式环境 配置

--- src/constants.ts
+++ src/constants.ts
@@ -23,4 +23,10 @@ export const LISTS = {
     logoURI: "https://dojo-token.netlify.app/images/tomo-test/0xc12c6CBbc9c26D73399bc430fA9AD4365d523Ab4.png",
     sort: false,
   },
+  "tomo": {
+    name: "tomo",
+    keywords: ["tomo", "default"],
+    logoURI: "https://dojo-token.netlify.app/images/tomo/0xB1f66997A5760428D3a87D68b90BfE0aE64121cC.png",
+    sort: false,
+  },
 } as const;
